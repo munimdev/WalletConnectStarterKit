ddf2ca19fe30539baacef175ae7583ed7778711a
munimdev <munimzafar100@gmail.com>
2022-12-10 00:14:11 +0500

    walletconnect modal

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
 
+import React from 'react';
+import Web3 from 'web3';
+import Web3Modal from "web3modal";
+import WalletConnect from "@walletconnect/client";
+import WalletConnectProvider from '@walletconnect/web3-provider'
+const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
+
+const INFURA_ID = '460f40a260564ac4a4f4b3fffb032dad'
+
+const providerOptions = {
+  walletconnect: {
+    package: WalletConnectProvider, // required
+    options: {
+      rpc : {
+        137: "https://matic-mainnet.chainstacklabs.com"
+      },
+      infuraId: INFURA_ID, // required
+    },
+  },
+};
+
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <button onClick={async () => {
+        const web3Modal = new Web3Modal({
+          network: "matic",
+          cacheProvider: false,
+          providerOptions,
+        });
+        const provider = await web3Modal.connect();
+        const web3 = new Web3(provider);
+
+        const accounts = await web3.eth.getAccounts();
+        console.log(accounts);
+      }}>Connect</button>
     </div>
+
+
   );
 }
 
